b2bef0e3fb77460351f11620d5da140dcc517fe6
Clearlove <348481519@qq.com>
2020-09-02 17:14:02 +0800

    完善模板样式

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>222</div>
+  <div class="content">ibos cli</div>
 </template>
 <script>
 export default {
 };
 </script>
 <style lang="scss">
+  .content {
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+    font-size: 24px;
+  }
 </style>
